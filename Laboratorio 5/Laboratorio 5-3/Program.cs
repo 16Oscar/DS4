cbd567fdf2d94dd1ba4e413be17a4454b14e2e71
OSCAR BARRABINO <oscar.barrabino@utp.ac.pa>
2024-09-11 19:45:34 -0500

    Laboratorio 5, Arreglos y Foreach

---
+++ Laboratorio 5/Laboratorio 5-3/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        String[] frutas = { "Manzana", "Plátano", "Naranja" };
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
---
+++ Laboratorio 5/Laboratorio 5/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program //Laboratorio 5-1
+{
+    private int[] sueldos;
+    public void Cargar()
+    {
+        sueldos = new int[6];
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("Ingrese sueldo del operador" + f + ": ");
+            String linea;
+            linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea); //Asignamos los 5 sueldos al vector
+        }
+    }
+    public void Imprimir()
+    {
+        Console.Write("Los 5 sueldos de los operadores \n");
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("["+ sueldos[f]+"]");
+        }
+        Console.ReadKey();
+    }
+
+    private static void Main(string[] args)
+    {
+        Program programa = new Program();
+
+        programa.Cargar();
+        programa.Imprimir();
+    }
+}
