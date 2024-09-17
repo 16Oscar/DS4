cf963522103c242e6caee0d388b6b1221c0a7746
OSCAR BARRABINO <oscar.barrabino@utp.ac.pa>
2024-09-16 19:11:16 -0500

    Laboratorio 6 - Excepciones

---
+++ Laboratorio 6/Laboratorio 6-3/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            int[] myNumbers = { 1, 2, 3 };
+        }
+        catch(Exception) {
+            Console.WriteLine("Algo salió mal, valide el índice del arreglo");
+        }
+        finally
+        {
+            Console.WriteLine("Continuación de la aplicación, luego del bloque try/catch");
+        }
+        
+    }
+}
---
+++ Laboratorio 6/Laboratorio 6/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program          //Laboratorio 6-1
+{
+    private static void Main(string[] args)
+    {
+        int num;
+
+        Console.WriteLine("Digite el número deseado: ");
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+        }
+        catch (FormatException ex) {
+            Console.WriteLine("No ha introducido un dígito válido");
+            num = -1;
+        }
+        Console.WriteLine(num);
+    }
+}
