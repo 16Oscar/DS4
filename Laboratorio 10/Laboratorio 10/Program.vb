a3b0b11e5ece7ac18b00c84cae3feaae92b9ed28
Oscar Barrabino <obarrabino@gmail.com>
2024-09-25 19:38:26 -0500

    Laboratorio 10 - Comparando C# y Visual Basic.NET

---
+++ Laboratorio 10/Laboratorio 10/Program.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hola")
+        Console.WriteLine("Este es mi primer programa de consola en VB")
+    End Sub
+End Module
